9490862367926cb94319f26920a0b91ab7e3bafd
abcdls0905 <1542971595@qq.com>
2017-07-15 11:07:01 +0800

    test

--- UnityGame/UnityGame/Assets/Game/LuaSource/main.lua
+++ UnityGame/UnityGame/Assets/Game/LuaSource/main.lua
@@ -14,7 +14,7 @@ function Main()
 	local panel = UIPackage.CreateObject(packageName, componentName)
 	local btn = panel:GetChild("Btn")
 	btn.onClick:Add(function()
-		print("----------------------------------------------------")
+		print("-----------------------------------------------------")
 	end)
 	print("btn", btn)
 
